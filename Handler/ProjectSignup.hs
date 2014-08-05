d9dfcf5d9dbae2cafe7b97451ccc1e5094b4cf17
Jason Harrer <jazzyeagle79@gmail.com>
2014-08-05 09:51:15 -0600

    Updated changes to ProjectSignup

--- Handler/ProjectSignup.hs
+++ Handler/ProjectSignup.hs
@@ -53,30 +53,31 @@ postProjectSignupR = do
     ((result, widget), enctype) <- runFormPost $ projectSignupForm 
     case result of
         FormSuccess project -> do
-          projectcheck <- select $ from $ \a -> do
-            where_ (a ^. ProjectSignupName ==. val (projectSignupName project) ||. ProjectName ==. val (projectSignupName project)
-            return count (a ^. ProjectSignupName)
+          projectcheck <- runDB $ select $ from $ \(a, p) -> do
+              where_ (a ^. ProjectSignupName ==. val (projectSignupName project) ||. p ^. ProjectName ==. val (projectSignupName project))
+              return $ count (a ^. ProjectSignupName)
           if projectcheck > 0
-            then defaultLayout $ do
-                setTitle "Project Signup Form: Submission Error! | Snowdrift.coop"
-                [whamlet|
-                    <H1 Align=Center>Submission Error
-                    <ul>
-                        <li color=red>#{projectSignupName $ project} already exists on Snowdrift.  You may have already submitted a sign-up request, which is still under review.
-                    <form method=GET action=@{ProjectSignupR} enctype=#{enctype}>
-                        <button>Return to previous form
-                |]
-            else defaultLayout $ do
-                runDB $ insert_ project 
-                setTitle "Project Signup Form: Success! | Snowdrift.coop"
-                [whamlet|
-                    <H1 Align=Center>Success!
-                    <br>
-                    <p>Your application to add #{projectSignupName $ project} has been submitted for review by the Snowdrift administrators to validate that your project meets the standards for Snowdrift.  You will receive a response within 7 - 10 business days.
-                    <p>If your application is approved, your project's default Snowdrift page will be created for you, at which point you can edit the content as you see fit.
-                    <p>If your application is rejected, you will receive an e-mail explaining why your project does not meet Snowdrift's criteria.  
-                    <H3 Align=Center>Thank you for your interest in Snowdrift!!
-                |]
+              then defaultLayout $ do
+                  setTitle "Project Signup Form: Submission Error! | Snowdrift.coop"
+                  [whamlet|
+                      <H1 Align=Center>Submission Error
+                      <ul>
+                          <li color=red>#{projectSignupName $ project} already exists on Snowdrift.  You may have already submitted a sign-up request, which is still under review.
+                      <form method=GET action=@{ProjectSignupR} enctype=#{enctype}>
+                          <button>Return to previous form
+                  |]
+              else do
+                  runDB $ insert_ project 
+                  defaultLayout $ do
+                      setTitle "Project Signup Form: Success! | Snowdrift.coop"
+                      [whamlet|
+                          <H1 Align=Center>Success!
+                          <br>
+                          <p>Your application to add #{projectSignupName $ project} has been submitted for review by the Snowdrift administrators to validate that your project meets the standards for Snowdrift.  You will receive a response within 7 - 10 business days.
+                          <p>If your application is approved, your project's default Snowdrift page will be created for you, at which point you can edit the content as you see fit.
+                          <p>If your application is rejected, you will receive an e-mail explaining why your project does not meet Snowdrift's criteria.  
+                          <H3 Align=Center>Thank you for your interest in Snowdrift!!
+                      |]
         FormFailure messages -> defaultLayout $ do
                 setTitle "Project Signup Form: Submission Error! | Snowdrift.coop"
                 [whamlet|

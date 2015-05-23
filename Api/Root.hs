f73daebadf15115231dfff0b49eb3aef91bd4a6c
Peter Harpending <peter@harpending.org>
2015-05-22 23:27:08 -0600

    /api -> GET returns API guide, POST minifies JSON

--- Api/Root.hs
+++ Api/Root.hs
@@ -2,5 +2,30 @@ module Api.Root where
 
 import Import
 
+import Control.Monad.Trans.Resource
+import Data.Aeson
+import Data.Conduit
+import qualified Data.Conduit.Attoparsec as CA
+import qualified Data.Conduit.Combinators as CC
+import Data.Default (def)
+import Data.FileEmbed
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Lazy as TL
+import Text.Markdown (markdown)
+
 getApiRootR :: Handler Html
-getApiRootR = error "Not yet implemented: getApiRootR"
+getApiRootR =
+  defaultLayout
+    (do let markdownBytes = $(embedFile "API.md")
+            markdownText =
+              TL.fromStrict (TE.decodeUtf8 markdownBytes)
+            htmlText = markdown def markdownText
+        toWidget htmlText)
+
+postApiRootR :: Handler RepJson
+postApiRootR =
+  do jsonValue <-
+       connect rawRequestBody (CA.sinkParser json)
+     fmap repJson
+          (respondSource typeJson
+                         (sendChunkLBS (encode jsonValue)))
